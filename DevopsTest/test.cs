da7e55f6db091f091f10a4ffc201bc206d09f6e8
kdh10704 <kdh10704@KDH_WORK>
2023-05-12 14:03:17 +0900

    새로만듬초기 커밋.

---
+++ DevopsTest/test.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Linq;
+namespace DevopsTest
+{
+
+    public class test
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("테스트한다.");
+        }
+    }
+}
